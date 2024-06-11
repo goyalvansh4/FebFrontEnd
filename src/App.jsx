41acce2b2a6bdc93ab2cba05aec0cd3aa7d0d8df
goyalvansh4 <vanshgupta7668@gmail.com>
2024-06-11 12:36:45 +0530

    Refactor App.jsx to remove conflicting code

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <h1 className='text-center'>Jai Shree Ram</h1>
-=======
       <Header />
->>>>>>> 6d568d5c7951c5d930795eca23a1e9b827f89d87
     </>
   )
 }
