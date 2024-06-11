6d568d5c7951c5d930795eca23a1e9b827f89d87
goyalvansh4 <vanshgupta7668@gmail.com>
2024-06-11 12:31:47 +0530

    Add responsive navigation menu to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-center'>Hellow World</h1>
+      <Header />
     </>
   )
 }
